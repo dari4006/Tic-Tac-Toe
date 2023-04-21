394a086e4f5d2056f832ac36a58c743d81709d44
dari4006 <darion.wong660@hotmail.com>
2023-04-21 13:42:28 +0930

    PROJECT / INPROGRESS

--- codeblocks.js
+++ codeblocks.js
@@ -1,8 +1,16 @@
 
 var section = document.querySelector('section')
 var player1 = true
-
+var player2 = false
+var counter = 0
 section.addEventListener("click", function () {
     var click = event.target
-    click.classList.add("crossin")
+    if (player1 === true && counter === 0)  
+    click.classList.add("cross-in")
+    var player1 = false
+    counter++
+    if (player1 === false && counter === 1)
+    click.classList.add("o-in")
+    var player1 = true
+    counter++
 })
